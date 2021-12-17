15d6c89403bb91f7d5f26e9c7d10cfaeea26bb57
Milos Zinajic <milos.zinajic@gmail.com>
2021-12-17 14:22:49 +0100

    Testiranje izmene

--- WebAPITest/Controllers/WeatherForecastController.cs
+++ WebAPITest/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebAPITest.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Test"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
