576a990242f041856ebd0bfa2c30cec4afb0b998
Milos Zinajic <milos.zinajic@gmail.com>
2021-12-17 15:24:54 +0100

    Swagger upgrade

--- WebAPITest/Startup.cs
+++ WebAPITest/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -44,6 +46,12 @@ namespace WebAPITest
                         Url = new Uri("https://coderjony.com/"),
                     },
                 });
+
+                #region XMl Documentation  
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+                #endregion
             });
         }
 
