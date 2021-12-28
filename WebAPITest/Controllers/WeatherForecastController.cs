6d0e0ede75590bb0ace40ed979a0e6d891d58a04
Milos Zinajic <milos.zinajic@gmail.com>
2021-12-28 13:15:02 +0100

    Dodat swaggerAnanas.yaml

--- WebAPITest/Controllers/WeatherForecastController.cs
+++ WebAPITest/Controllers/WeatherForecastController.cs
@@ -35,5 +35,18 @@ namespace WebAPITest.Controllers
             })
             .ToArray();
         }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get1()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
     }
 }
